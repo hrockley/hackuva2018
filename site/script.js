0b7990c1432e5906106876ced1e188a89d5e8783
Harun Feraidon <harunferaidon@gmail.com>
2018-03-24 21:17:42 -0400

    map as background

---
+++ site/script.js
@@ -0,0 +1,24 @@
+
+function initMap() {
+  var uluru = {lat: -25.363, lng: 131.044};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
+
+$(function(){
+  changeHeight();
+
+
+  function changeHeight(){
+    let HEIGHT = $(window).height() + "px";
+
+    console.log(HEIGHT);
+    $("#map").css("height", HEIGHT);
+  }
+})
